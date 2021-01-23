1d6209098fad07f804de39fd992a2c88ffaf146c
Christian Kischy Haege <kischy@gmx.de>
2021-01-23 20:48:25 +0100

    Added project files

---
+++ p26-euler-Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace p26_euler_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ p26-euler/p26-euler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace p26_euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculation started");
+
+
+            int answer_p26 = 0;
+
+            Console.WriteLine("The answer to problem 26 of project Euler is " + answer_p26 + ".");
+        }
+    }
+}
