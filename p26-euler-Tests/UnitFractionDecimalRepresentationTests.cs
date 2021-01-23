5e34b4c5ca3145ce0ffd7e2133a0c1831bde6aa7
Christian Kischy Haege <kischy@gmx.de>
2021-01-23 21:03:15 +0100

    Added Tests for denominator 2-10

---
+++ p26-euler-Tests/UnitFractionDecimalRepresentationTests.cs
@@ -0,0 +1,114 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p26_euler;
+
+namespace p26_euler_Tests
+{
+    [TestClass]
+    public class UnitFractionDecimalRepresentationTests
+    {
+
+        UnitFractionDecimalRepresentation ufdr = new UnitFractionDecimalRepresentation(2);
+
+        [TestMethod]
+        public void TestOneHalf()
+        {
+            ufdr.Denominator = 2;
+
+            Assert.AreEqual(5, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_3()
+        {
+            ufdr.Denominator = 3;
+
+            Assert.AreEqual(3, ufdr.GetNextDigit());
+            Assert.AreEqual(3, ufdr.GetNextDigit());
+            Assert.AreEqual(3, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_4()
+        {
+            ufdr.Denominator = 4;
+
+            Assert.AreEqual(2, ufdr.GetNextDigit());
+            Assert.AreEqual(5, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_5()
+        {
+            ufdr.Denominator = 5;
+
+            Assert.AreEqual(2, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_6()
+        {
+            ufdr.Denominator = 6;
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(6, ufdr.GetNextDigit());
+            Assert.AreEqual(6, ufdr.GetNextDigit());
+            Assert.AreEqual(6, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_7()
+        {
+            ufdr.Denominator = 7;
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(4, ufdr.GetNextDigit());
+            Assert.AreEqual(2, ufdr.GetNextDigit());
+            Assert.AreEqual(8, ufdr.GetNextDigit());
+            Assert.AreEqual(5, ufdr.GetNextDigit());
+            Assert.AreEqual(7, ufdr.GetNextDigit());
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(4, ufdr.GetNextDigit());
+            Assert.AreEqual(2, ufdr.GetNextDigit());
+            Assert.AreEqual(8, ufdr.GetNextDigit());
+            Assert.AreEqual(5, ufdr.GetNextDigit());
+            Assert.AreEqual(7, ufdr.GetNextDigit());
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(4, ufdr.GetNextDigit());
+            Assert.AreEqual(2, ufdr.GetNextDigit());
+            Assert.AreEqual(8, ufdr.GetNextDigit());
+            Assert.AreEqual(5, ufdr.GetNextDigit());
+            Assert.AreEqual(7, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_8()
+        {
+            ufdr.Denominator = 8;
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(2, ufdr.GetNextDigit());
+            Assert.AreEqual(5, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_9()
+        {
+            ufdr.Denominator = 9;
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+        }
+
+        public void Test1_div_10()
+        {
+            ufdr.Denominator = 10;
+
+            Assert.AreEqual(1, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+            Assert.AreEqual(-1, ufdr.GetNextDigit());
+        }
+    }
+}
---
+++ p26-euler/UnitFractionDecimalRepresentation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p26_euler
+{
+    public class UnitFractionDecimalRepresentation
+    {
+
+        private int denominator;
+
+        public int Denominator 
+        {
+            get
+            {
+                return denominator;
+            }
+
+            set
+            {
+                if(value < 2)
+                {
+                    throw new ArgumentException("Value must be at least 2 or higher.");
+                }
+
+                denominator = value;
+            }
+
+             
+        }
+
+        public UnitFractionDecimalRepresentation(int denom)
+        {
+            Denominator = denom;
+        }
+
+        public int GetNextDigit()
+        {
+            return 1;
+        }
+
+
+    }
+}
